1e0893d268d97969d40f2f74cd43f55748224c6b
DrFlask <rje4242@gmail.com>
2023-08-17 18:16:33 -0700

    show graph
    display graph with streamlit

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
 import streamlit as st
 
-st.title("GPT-Squad test streamlit app")
-st.write("This is the first modification.")
+# st.title("Welcome to Health Universe!")
+# st.write("This is a sample application.")
 
+# Create a simple Matplotlib plot
+fig, ax = plt.subplots()
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 15, 30, 25]
+ax.plot(x, y)
+
+# Display the Matplotlib plot using Streamlit
+st.pyplot(fig)
